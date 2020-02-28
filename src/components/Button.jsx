137ff5a72cd4aef99cf4c09f99aa0805b7f82cad
arthur soares pinto <arthur.soares@external.facgroup.com>
2020-02-28 20:27:03 -0300

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Button.css'
+
+// onClick={e => props.click && props.click(props.label)}
+export default props =>{
+    let classes = 'button '
+    classes += props.operation ? 'operation':''
+    classes += props.double ? 'double':''
+    classes += props.triple ? 'triple':''
+
+    return(
+        <button
+            onClick={e => props.click && props.click(e.target.innerHTML)}
+            className={classes}>
+            {props.label}
+        </button>
+
+    )
+}
